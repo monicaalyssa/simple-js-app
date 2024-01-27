b17cda88b0c3e4c8d908bef746dd789843297f08
Monica Alyssa <149343926+monicaalyssa@users.noreply.github.com>
2024-01-27 18:44:46 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+alert('Hello World');
+
+let myName = 'Monica';
+let myAge = 21;
+
+
+// This is my updated name
+myName = 'Monica Alyssa'
+
+document.write(myName);
+
+
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+/* Doubles the size of doubleSize then subtracts it by a value 
+which is the quotient of the original size divded by 2 */
+
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
