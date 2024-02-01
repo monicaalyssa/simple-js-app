3d32444a78d8fd04954625b05de90cd4c3e3e3f8
Monica Alyssa <149343926+monicaalyssa@users.noreply.github.com>
2024-01-31 22:50:07 -0500

    Added Pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,5 @@
-alert('Hello World');
-
-let myName = 'Monica';
-let myAge = 21;
-
-
-// This is my updated name
-myName = 'Monica Alyssa'
-
-document.write(myName);
-
-
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-/* Doubles the size of doubleSize then subtracts it by a value 
-which is the quotient of the original size divded by 2 */
-
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-
-
-let favoriteFood = 'Pasta';
-document.write(favoriteFood);
+let pokemonList = [ 
+    { name: 'Pichu', height: 1, type: 'Electric'},
+    { name: 'Marill', height: 1.4, type: ['Water', 'Fairy']},
+    { name: 'Natu', height: 0.8, type: ['Psychihc', 'Flying']}
+];
