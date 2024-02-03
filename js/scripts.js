de5cb860f8b151716b9e0b1fb861e2b30cfbf2f6
Monica Alyssa <149343926+monicaalyssa@users.noreply.github.com>
2024-02-03 01:31:35 -0500

    Added loops and conditionals for displaying Pokemon's

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,26 @@
 let pokemonList = [ 
-    { name: 'Pichu', height: 1, type: 'Electric'},
-    { name: 'Marill', height: 1.4, type: ['Water', 'Fairy']},
-    { name: 'Natu', height: 0.8, type: ['Psychihc', 'Flying']}
+    { 
+        name: 'Pichu', 
+        height: 1, 
+        type: 'Electric'
+    },
+    { 
+        name: 'Marill', 
+        height: 1.4, 
+        type: ['Water', 'Fairy']
+    },
+    { 
+        name: 'Natu', 
+        height: 0.8, 
+        type: ['Psychihc', 'Flying']
+    }
 ];
+
+for (let i = 0 ; i < pokemonList.length; i++) {
+    document.write(pokemonList[i].name + " (Height: " + pokemonList[i].height + ") ")
+    
+    if (pokemonList[i].height > 1.2) {
+        document.write(" - Wow that's big! ")
+    }
+}
+
