d62c0438e761cdfa5b15e360d5751de6eea161c4
Monica Alyssa <149343926+monicaalyssa@users.noreply.github.com>
2024-02-26 15:41:37 -0500

    Added IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,38 @@
-let pokemonList = [ 
-    { 
-        name: 'Pichu', 
-        height: 1, 
-        type: 'Electric'
-    },
-    { 
-        name: 'Marill', 
-        height: 1.4, 
-        type: ['Water', 'Fairy']
-    },
-    { 
-        name: 'Natu', 
-        height: 0.8, 
-        type: ['Psychihc', 'Flying']
-    }
-];
 
-pokemonList.forEach(function(pokemon) {
+let pokemonRepository = (function () {
+
+    let pokemonList = [
+        {
+            name: 'Pichu',
+            height: 1,
+            type: 'Electric'
+        },
+        {
+            name: 'Marill',
+            height: 1.4,
+            type: ['Water', 'Fairy']
+        },
+        {
+            name: 'Natu',
+            height: 0.8,
+            type: ['Psychihc', 'Flying']
+        }
+    ];
+
+    return {
+        add: function(pokemon) {
+            pokemonList.push(pokemon);
+        },
+        getAll: function() {
+            return pokemonList;
+        }
+    };
+
+})();
+
+pokemonRepository.getAll().forEach(function (pokemon) {
     document.write("<br>" + pokemon.name, " (Height: " + pokemon.height + ")")
     if (pokemon.height > 1.2) {
         document.write(" — Wow that's big!")
-        }
-    });
-
-pokemonList.forEach();
+    }
+});
